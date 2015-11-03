d09ac51d7c7b64af3ad0373c69a12f6cfccfd6f5
Evans Mwendwa <evansmwendwa@live.com>
2015-11-03 17:40:19 +0300

    added file upload handler

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -17,5 +19,34 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/image", name="postImage")
+     */
+    public function postImage(Request $request)
+    {
+        $uploadPath = realpath(__DIR__.'/../../../web/media');
+
+        $file = $request->files->get('file');
+
+        $data = array(
+            'status'=> 200,
+            'success'=> false,
+            'message'=> 'no file selected',
+            'file'=> null
+        );
+
+        if($file){
+            $newFileName = md5($file->getClientOriginalName().time() ) ."." . $file->guessExtension();
+
+            $file->move($uploadPath,$newFileName);
+
+            $data['success'] = true;
+            $data['message'] = 'File Uploaded';
+            $data['file'] = "/media/".$newFileName;
+        }
+
+        return new JsonResponse($data);
     }
 }
